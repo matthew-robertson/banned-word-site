bd0ba9b1db03f6ae37d54f0eaa61de3ec4e93f90
matthew-robertson <m33rober@uwaterloo.ca>
2020-03-10 20:00:44 -0400

    Rips out Admin Stuff
    It was poorly thought out stuff with no authentication, just done before
    OAuth worked. Not needed now.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import { Route, Switch } from 'react-router-dom'
 
 import LandingPage from './pages/LandingPage'
 import UserRootPage from './pages/dashboard/UserRootPage'
-import AdminRootPage from './pages/adminpanel/AdminRootPage'
-import AdminServersPage from './pages/adminpanel/AdminServersPage'
-import AdminServerPage from './pages/adminpanel/AdminServerPage'
 import UserServerPage from './pages/dashboard/UserServerPage'
 import Header from './components/Header'
 import './App.css';
@@ -18,9 +15,6 @@ function App() {
 	      <Route exact path="/" component={LandingPage} />
 	      <Route exact path="/servers/:server_id" component={UserServerPage} />
 	      <Route exact path="/user" component={UserRootPage} />
-	      <Route exact path="/admin" component={AdminRootPage} />
-	      <Route exact path="/admin/servers" component={AdminServersPage} />
-	      <Route exact path="/admin/servers/:server_id" component={AdminServerPage} />
 	    </Switch>
 	  </div>
   );
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -8,9 +8,6 @@ export default function LandingPage() {
   return (
     <div className="container">
       <h1>Home Page</h1>
-      <p>
-        <Link to="/admin">Admin Panel</Link>
-      </p>
       <button onClick={() => auth.login().then(a => console.log(a))}>
       	Sign in to Discord
       </button>
